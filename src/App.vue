c9fd61fd5848f3f359d21d099c06a685bb037d18
Jeremy Nathan <jeremy.nathan1@gmail.com>
2025-08-26 22:17:36 +0800

    feat: Add Title component and center content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { Button } from '@/components/ui/button'
+import Title from '@/components/Title.vue'
 </script>
 
 <template>
-  <div>
-    <Button>Click me</Button>
+  <div class="min-h-screen p-16">
+    <div class="max-w-4xl mx-auto">
+      <Title class="mb-10" />
+      <Button>Click me</Button>
+    </div>
   </div>
-  <p>Test</p>
 </template>
 
 <style>
