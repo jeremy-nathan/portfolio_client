53d676f2e15777911da183d9773e91e1154e52a9
Jeremy Nathan <jeremy.nathan1@gmail.com>
2025-08-26 22:16:29 +0800

    chore: Optimize server configuration
    Simplify the server configuration in vite.config.js by removing
    the strictPort and hmr.clientPort options. reduces
    the complexity of the configuration and aligns with the default
    behavior of Vite, which should work well for the project.

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true,
-    hmr: {
-      clientPort: 5173,
-    },
   },
   resolve: {
     alias: {
